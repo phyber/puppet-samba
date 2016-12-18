689d67434abd242f5f2de954fa337ac70b5accf4
David O'Rourke <david.orourke@xm0.uk>
2016-12-18 23:58:56 +0000

    server::params: add service start/stop/status commands.
    Needed for FreeBSD, as the default Puppet one* service commands result
    in issues when starting the service with winbindd disabled.

--- manifests/server/params.pp
+++ manifests/server/params.pp
@@ -3,70 +3,91 @@
 class samba::server::params {
   case $::osfamily {
     'Redhat': {
-      $service_name  = 'smb'
-      $package_name  = 'samba'
-      $config_file   = '/etc/samba/smb.conf'
-      $config_group  = 'root'
-      $script_prefix = '/sbin'
-      $pdbedit       = '/usr/bin/pdbedit'
+      $service_name   = 'smb'
+      $service_start  = undef
+      $service_status = undef
+      $service_stop   = undef
+      $package_name   = 'samba'
+      $config_file    = '/etc/samba/smb.conf'
+      $config_group   = 'root'
+      $script_prefix  = '/sbin'
+      $pdbedit        = '/usr/bin/pdbedit'
     }
     'Debian': {
       case $::operatingsystem {
         'Debian': {
           case $::operatingsystemmajrelease {
             '8' : {
-              $service_name  = 'smbd'
-              $package_name  = 'samba'
-              $config_file   = '/etc/samba/smb.conf'
-              $config_group  = 'root'
-              $script_prefix = '/sbin'
-              $pdbedit       = '/usr/bin/pdbedit'
+              $service_name   = 'smbd'
+              $service_start  = undef
+              $service_status = undef
+              $service_stop   = undef
+              $package_name   = 'samba'
+              $config_file    = '/etc/samba/smb.conf'
+              $config_group   = 'root'
+              $script_prefix  = '/sbin'
+              $pdbedit        = '/usr/bin/pdbedit'
             }
             default: {
               $service_name  = 'samba'
-              $package_name  = 'samba'
-              $config_file   = '/etc/samba/smb.conf'
-              $config_group  = 'root'
-              $script_prefix = '/sbin'
-              $pdbedit       = '/usr/bin/pdbedit'
+              $service_start  = undef
+              $service_status = undef
+              $service_stop   = undef
+              $package_name   = 'samba'
+              $config_file    = '/etc/samba/smb.conf'
+              $config_group   = 'root'
+              $script_prefix  = '/sbin'
+              $pdbedit        = '/usr/bin/pdbedit'
             }
           }
         }
         'Ubuntu': {
-          $service_name  = 'smbd'
-          $package_name  = 'samba'
-          $config_file   = '/etc/samba/smb.conf'
-          $config_group  = 'root'
-          $script_prefix = '/sbin'
-          $pdbedit       = '/usr/bin/pdbedit'
-          $nmbd_name     = 'nmbd'
+          $service_name   = 'smbd'
+          $service_start  = undef
+          $service_status = undef
+          $service_stop   = undef
+          $package_name   = 'samba'
+          $config_file    = '/etc/samba/smb.conf'
+          $config_group   = 'root'
+          $script_prefix  = '/sbin'
+          $pdbedit        = '/usr/bin/pdbedit'
+          $nmbd_name      = 'nmbd'
         }
         default: {
-          $service_name  = 'samba'
-          $package_name  = 'samba'
-          $config_file   = '/etc/samba/smb.conf'
-          $config_group  = 'root'
-          $script_prefix = '/sbin'
-          $pdbedit       = '/usr/bin/pdbedit'
+          $service_name   = 'samba'
+          $service_start  = undef
+          $service_status = undef
+          $service_stop   = undef
+          $package_name   = 'samba'
+          $config_file    = '/etc/samba/smb.conf'
+          $config_group   = 'root'
+          $script_prefix  = '/sbin'
+          $pdbedit        = '/usr/bin/pdbedit'
         }
       }
     }
     'Gentoo': {
-      $service_name  = 'samba'
-      $package_name  = 'samba'
-      $config_file   = '/etc/samba/smb.conf'
-      $config_group  = 'root'
-      $script_prefix = '/sbin'
-      $pdbedit       = '/usr/bin/pdbedit'
+      $service_name   = 'samba'
+      $service_start  = undef
+      $service_status = undef
+      $service_stop   = undef
+      $package_name   = 'samba'
+      $config_file    = '/etc/samba/smb.conf'
+      $config_group   = 'root'
+      $script_prefix  = '/sbin'
+      $pdbedit        = '/usr/bin/pdbedit'
     }
     'Archlinux': {
-      $service_name  = 'smbd'
-      $package_name  = 'samba'
-      $config_file   = '/etc/samba/smb.conf'
-      $config_group  = 'root'
-      $script_prefix = '/sbin'
-      $pdbedit       = '/usr/bin/pdbedit'
-      $nmbd_name     = 'nmbd'
+      $service_name   = 'smbd'
+      $service_start  = undef
+      $service_status = undef
+      $service_stop   = undef
+      $package_name   = 'samba'
+      $config_file    = '/etc/samba/smb.conf'
+      $config_group   = 'root'
+      $script_prefix  = '/sbin'
+      $pdbedit        = '/usr/bin/pdbedit'
+      $nmbd_name      = 'nmbd'
     }
 
     # Currently Gentoo has $::osfamily = "Linux". This should change in
@@ -75,23 +96,29 @@ class samba::server::params {
     'Linux': {
       case $::operatingsystem {
         'Gentoo':  {
-          $service_name  = 'samba'
-          $package_name  = 'samba'
-          $config_file   = '/etc/samba/smb.conf'
-          $config_group  = 'root'
-          $script_prefix = '/sbin'
-          $pdbedit       = '/usr/bin/pdbedit'
+          $service_name   = 'samba'
+          $service_start  = undef
+          $service_status = undef
+          $service_stop   = undef
+          $package_name   = 'samba'
+          $config_file    = '/etc/samba/smb.conf'
+          $config_group   = 'root'
+          $script_prefix  = '/sbin'
+          $pdbedit        = '/usr/bin/pdbedit'
         }
         default: { fail("${::operatingsystem} is not supported by this module.") }
       }
     }
     'FreeBSD': {
-        $service_name  = 'samba_server'
-        $package_name  = 'net/samba44'
-        $config_file   = '/usr/local/etc/smb4.conf'
-        $config_group  = 'wheel'
-        $script_prefix = '/usr/local/sbin'
-        $pdbedit       = '/usr/local/bin/pdbedit'
+        $service_name   = 'samba_server'
+        $service_start  = "service ${service_name} start"
+        $service_status = "service ${service_name} status"
+        $service_stop   = "service ${service_name} stop"
+        $package_name   = 'net/samba44'
+        $config_file    = '/usr/local/etc/smb4.conf'
+        $config_group   = 'wheel'
+        $script_prefix  = '/usr/local/sbin'
+        $pdbedit        = '/usr/local/bin/pdbedit'
     }
     default: { fail("${::osfamily} is not supported by this module.") }
   }
