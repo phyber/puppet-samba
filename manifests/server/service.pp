3ce5184beed113823f172f58f38be387bdf767bc
David O'Rourke <david.orourke@xm0.uk>
2016-12-19 00:01:36 +0000

    server::service: Use start/stop/status from params.

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -3,18 +3,21 @@
 class samba::server::service (
   $ensure = running,
   $enable = true
-) inherits samba::server::params {
+) inherits ::samba::server::params {
 
-  service { $samba::server::params::service_name :
+  service { $::samba::server::params::service_name :
     ensure     => $ensure,
     hasstatus  => true,
     hasrestart => true,
     enable     => $enable,
+    start      => $::samba::server::params::service_start,
+    status     => $::samba::server::params::service_status,
+    stop       => $::samba::server::params::service_stop,
     require    => Class['samba::server::config']
   }
 
-  if $samba::server::params::nmbd_name != undef {
-    service { $samba::server::params::nmbd_name :
+  if $::samba::server::params::nmbd_name != undef {
+    service { $::samba::server::params::nmbd_name :
       ensure     => $ensure,
       hasrestart => false,
       enable     => $enable,
